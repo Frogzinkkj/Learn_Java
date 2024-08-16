3d8bd0504f6a40a6b485f4496bc6ee3b9a1e564e
Frogzin <andre.pereira@ucsal.edu.br>
2024-08-16 14:41:27 +0000

    Basic Sintax

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        char letra = 'p';
+        String name = String.valueOf(letra);
+        letra = name.charAt(0);
+        int idade =19;
+        String age = String.valueOf(idade);
+        idade = Integer.parseInt(age);
+    }
+}

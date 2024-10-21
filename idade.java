04157841ce28eab5c3e9b6d5b48ea7435fe1a6ca
Frogzin <andre.pereira@ucsal.edu.br>
2024-10-21 14:21:19 +0000

    adicionando o resto

---
+++ idade.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class idade{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int idade;
+
+    System.out.println("Digite sua idade");
+    idade = input.nextInt();
+    verifica(idade);
+    input.close();
+  }
+  public static void verifica (int a){
+    if (a <=15){
+      System.out.println("Voce nao pode votar");
+
+    }
+    else{
+      if((a >= 18) && (a <= 64)){
+        System.out.println("Voto obrigatorio");
+      }
+      else{
+        System.out.println("Voto facultativo");
+      }
+    }
+    
+  }
+}
---
+++ mod.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class mod {
+
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+    String nome;
+    int nota1,nota2,nota3,nota4,qnt_aluno,media;
+    int cot=1;
+    
+    System.out.println("Digite a quantidade de Alunos da Sala: ");
+    qnt_aluno = input.nextInt();
+
+    while (cot<=qnt_aluno) {
+          
+      System.out.println("Digite o nome do aluno"+cot);
+      input.nextLine();
+      nome = input.nextLine();
+      System.out.println("Digite a Primeira nota: ");
+      nota1 = input.nextInt();
+      System.out.println("Digite a Segunda nota: ");
+      nota2 = input.nextInt();
+      System.out.println("Digite a Terceira nota: ");
+      nota3 = input.nextInt();
+      System.out.println("Digite a Quarta nota: ");
+      nota4 = input.nextInt();
+      cot ++;
+      media = (nota1+nota2+nota3+nota4)/4;
+      fato(nome, media);
+    }
+    input.close();
+  }
+
+  public static void fato(String nome , double md){
+    System.out.println("Nome do Aluno: "+nome);
+    System.out.println("Media do Aluno "+md);
+    if (md>=7) {
+      System.out.println("Parabens, "+nome+" voce foi aprovado");
+
+
+      
+    } else {
+      if (md<4) {
+        System.out.println(nome+" voce nao alcançou a media minima");
+        
+      } else {
+        System.out.println(nome+" ficou no meio");
+        
+      }
+      
+    }
+
+  }
+}

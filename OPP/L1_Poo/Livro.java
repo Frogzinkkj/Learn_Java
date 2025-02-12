386a5ce03737c42ace9bfbfdba83fb42b4716250
andre.pereira@ucsal.edu.br <andre.pereira@ucsal.edu.br>
2025-02-12 17:30:32 -0300

    updates from the book quest

--- OPP/L1_Poo/Livro.java
+++ OPP/L1_Poo/Livro.java
@@ -18,16 +18,25 @@ public class Livro {
   }
 
 
+  
+  //Um método que aplica desconto ao preço do livro.
+  
+  public double priceOff(){
+    
+    return price*0.75;
+    
+  }
   //Adicione métodos para exibir os detalhes do livro.//
 
-  public void ShowDetails(){
+  public void showDetails(){
     
     System.out.println("This Book Title is " + title);
     System.out.println("This Book Autor is " + autor);
     System.out.println("This Book Published in " + publicateYear);
-    System.out.println("This Book Price is " + price);
-  }
+    System.out.printf("This Book Price with cupom is: $%.2f\n", priceOff());
 
+  }
+  
   
   
   //Adicione os métodos getters e setters para cada atributo da classe Livro.//
@@ -76,7 +85,7 @@ public class Livro {
   
     Livro MyFavoriteBook = new Livro("Little Prince", "Antoine de Saint", 2024, 5.00);
 
-    MyFavoriteBook.ShowDetails();
+    MyFavoriteBook.showDetails();
   }
 
 
